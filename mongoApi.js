583cd0e24f3340a349782ea3b0b34207dbc2c670
Jennifer Gommans <jennifer.gommans@codecentric.de>
2020-12-28 15:56:15 +0100

    add initial graphql schema for tictactoe users

---
+++ mongoApi.js
@@ -0,0 +1,62 @@
+const MongoClient = require('mongodb').MongoClient;
+const mongoUrl = "mongodb://localhost:27017/";
+
+const ticTacToeDatabase = "ticTacToe";
+const userCollection = "user";
+
+export const getAllUser = async () => {
+    let mongoClient, db;
+    try {
+        mongoClient = await MongoClient.connect(mongoUrl, {useNewUrlParser: true});
+        db = mongoClient.db(ticTacToeDatabase);
+        const coll = db.collection(userCollection);
+        return await coll.find().toArray();
+    } catch (e) {
+        console.error(e)
+    } finally {
+        await mongoClient.close();
+    }
+};
+
+export const getUserByUsername = async (name) => {
+    let mongoClient, db;
+    try {
+        mongoClient = await MongoClient.connect(mongoUrl, {useNewUrlParser: true});
+        db = mongoClient.db(ticTacToeDatabase);
+        const coll = db.collection(userCollection);
+        return await coll.findOne({name});
+    } catch (e) {
+        console.error(e)
+    } finally {
+        await mongoClient.close();
+    }
+};
+
+export const createUser = async (name) => {
+    let mongoClient, db;
+    try {
+        mongoClient = await MongoClient.connect(mongoUrl, {useNewUrlParser: true});
+        db = mongoClient.db(ticTacToeDatabase);
+        const coll = db.collection(userCollection);
+        const id = require('crypto').randomBytes(10).toString('hex');
+        return await coll.insertOne({id, name, score: 1});
+    } catch (e) {
+        console.error(e)
+    } finally {
+        await mongoClient.close();
+    }
+};
+
+export const updateUser = async (name, score) => {
+    let mongoClient, db;
+    try {
+        mongoClient = await MongoClient.connect(mongoUrl, {useNewUrlParser: true});
+        db = mongoClient.db(ticTacToeDatabase);
+        const coll = db.collection(userCollection);
+        return await coll.findOneAndUpdate({name}, {score});
+    } catch (e) {
+        console.error(e)
+    } finally {
+        await mongoClient.close();
+    }
+};
---
+++ server.js
@@ -0,0 +1,71 @@
+import {getAllUser, getUserByUsername} from "./mongoApi";
+import express from "express";
+import {graphqlHTTP} from "express-graphql";
+import {buildSchema} from "graphql";
+
+const schema = buildSchema(`
+  type User {
+    id: ID!
+    name: String
+    score: Int
+  }
+
+  type Query {
+    users: [User]
+  }
+
+  type Mutation {
+    increaseScore(name: String): User
+  }
+`);
+
+class User {
+    constructor(id, name, score) {
+        this.id = id;
+        this.name = name;
+        this.score = score;
+    }
+}
+
+let userList = [];
+
+const root = {
+    // {
+    //   users {
+    //     id,
+    //     name
+    //     score
+    //   }
+    // }
+    users: async () => {
+        return await getAllUser();
+    },
+    // mutation {
+    //     increaseScore(name: "maluc") {
+    //         id
+    //         name
+    //         score
+    //     }
+    // }
+    increaseScore: async ({name}) => {
+        const existing = userList.findIndex(user => user.name === name);
+        if (existing !== -1) {
+            userList[existing].score++;
+            return userList[existing];
+        }
+        const id = require('crypto').randomBytes(10).toString('hex');
+        const newUser = new User(id, name, 1);
+        userList.push(newUser);
+        return newUser
+    }
+};
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+app.listen(4000, () => {
+    console.log('Running a GraphQL API server at localhost:4000/graphql');
+});
