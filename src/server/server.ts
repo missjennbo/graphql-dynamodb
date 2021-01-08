b4d0212801c5aee9e6c84a0176468073deec9dbf
Jennifer Lucifero <jennifer.gommans@codecentric.de>
2021-01-08 14:46:53 +0100

    Undo port change

--- src/server/server.ts
+++ src/server/server.ts
@@ -53,6 +53,6 @@ app.use(
         graphiql: true,
     })
 );
-app.listen(80, () => {
-    console.log('Running a GraphQL API server at localhost:80/graphql');
+app.listen(4000, () => {
+    console.log('Running a GraphQL API server at localhost:4000/graphql');
 });
