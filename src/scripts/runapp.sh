4a067bde53c901c1d76499808d470a0910ae9d28
Jennifer Lucifero <jennifer.gommans@codecentric.de>
2021-02-15 11:24:54 +0100

    Fix path to server.js in runapp script

--- src/scripts/runapp.sh
+++ src/scripts/runapp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cp /home/ubuntu/tictactoe/tictactoe.service /etc/systemd/system
+sudo cp /home/ubuntu/tictactoe/tictactoe.service /etc/systemd/system
 ##add exceutable permissions to express app
-sudo chmod +x /home/ubuntu/tictactoe/src/server/server.js
+sudo chmod +x /home/ubuntu/tictactoe/build/server/server.js
 ##Allows any users to write the app folder. Useful if using fs within the app
 sudo chmod go+w /home/ubuntu/tictactoe
 ##Launches the express app
